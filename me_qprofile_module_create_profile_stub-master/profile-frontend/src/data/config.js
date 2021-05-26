c2abfc44046fa1731fa09fa40844ca8347613687
Nimmagadda Sai Prathyusha <nimmagaddasaiprathyusha@gmail.com>
2021-05-26 18:07:58 +0530

    Initial commit

---
+++ me_qprofile_module_create_profile_stub-master/profile-frontend/src/data/config.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  // TODO: change your profile information here
+  name: "Prathyusha",
+  greeting: "Hey 👋",
+  greetingDescription: "I'm Sai Prathyusha. I'm an IoT Enthusiast, Blogger and Web 🕸 Developer!",
+  githubUrl: "https://github.com/Prathyusha2210",
+  linkedinUrl: "https://www.linkedin.com/in/sai-prathyusha/",
+  cvLink: "https://drive.google.com/file/d/1_iDlQqB-yvtAIXzFDWw_id7hEhSEepSm/view?usp=sharing",
+};
